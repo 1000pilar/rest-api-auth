0b1560705c33e7e1b7ade541246f8e7732e38134
1000pilar <nunua7a8@gmailcom>
2017-05-22 19:14:18 +0700

    heroku run

--- controllers/users.js
+++ controllers/users.js
@@ -4,55 +4,56 @@ var jwt = require('jsonwebtoken')
 var passwordHash = require('password-hash')
 
 module.exports = {
-  getAllUser : (req, res)=>{
-    models.User.findAll()
-    .then ((users)=>{
-      res.send(users)
-    })
-  },
-
-  findUser : (req, res)=>{
-    models.User.findById(req.params.id)
-    .then ((user)=>{
-      res.send(user)
-    })
-  },
-
-  insertUser : (req, res)=>{
-    models.User.create({
-      name: req.body.name,
-      username: req.body.username,
-      password: req.body.password
-    })
-    .then ((user)=>{
-      res.send(user)
-    })
-  },
-
-  deleteUser: (req, res)=>{
-    models.User.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-    .then ((msg)=>res.send({msg: `You have deleted id ${req.params.id}`}))
-  },
-
-  updateUser: (req, res)=>{
-    let user = {
-      name: req.body.name,
-      username: req.body.username,
-      password: req.body.password
-    }
-    models.User.update(user, {
-      where:{
-        id: req.params.id
-      }
-    })
-    .then(()=>{
-      res.send(`has been change to `+ req.body.name )
-    })
-  },
+  // getAllUser : (req, res)=>{
+  //   models.User.findAll()
+  //   .then ((users)=>{
+  //     res.send(users)
+  //   })
+  // },
+  //
+  // findUser : (req, res)=>{
+  //   models.User.findById(req.params.id)
+  //   .then ((user)=>{
+  //     res.send(user)
+  //   })
+  // },
+  //
+  // insertUser : (req, res)=>{
+  //   models.User.create({
+  //     name: req.body.name,
+  //     username: req.body.username,
+  //     password: req.body.password,
+  //     role: req.body.role
+  //   })
+  //   .then ((user)=>{
+  //     res.send(user)
+  //   })
+  // },
+  //
+  // deleteUser: (req, res)=>{
+  //   models.User.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   })
+  //   .then ((msg)=>res.send({msg: `You have deleted id ${req.params.id}`}))
+  // },
+  //
+  // updateUser: (req, res)=>{
+  //   let user = {
+  //     name: req.body.name,
+  //     username: req.body.username,
+  //     password: req.body.password
+  //   }
+  //   models.User.update(user, {
+  //     where:{
+  //       id: req.params.id
+  //     }
+  //   })
+  //   .then(()=>{
+  //     res.send(`Data id ${req.params.id} Has been changed`)
+  //   })
+  // },
 
   signUp: (req, res)=>{
     var password = passwordHash.generate(req.body.password)
@@ -91,8 +92,8 @@ module.exports = {
 
   getAllUserInfo: (req, res)=>{
     var token = jwt.verify(req.headers.token, 'krisis_only_need_to_handle_with_calm');
-    console.log(token.role);
-    if (token.role === "er"){
+    // console.log(token.role);
+    if (token.role.toLowerCase() === "admin"){
       models.User.findAll()
       .then((users)=>{
         res.send(users)
@@ -104,7 +105,7 @@ module.exports = {
 
   getAuthor: (req, res)=>{
     var token = jwt.verify(req.header.token, 'krisis_only_need_to_handle_with_calm');
-    if(token.role === 'admin' || token.role === 'user'){
+    if(token.role.toLowerCase() === 'admin' || token.role.toLowerCase() === 'user'){
       models.User.findById(req.params.id)
       .then ((user)=>{
         res.send(user)
@@ -116,7 +117,7 @@ module.exports = {
 
   insertUserByAdmin: (req, res)=>{
     var token = jwt.verify(req.header.token, 'krisis_only_need_to_handle_with_calm');
-    if(token.role === 'admin'){
+    if(token.role.toLowerCase() === 'admin'){
       var password = passwordHash.generate(req.body.password)
       models.User.create({
         name: req.body.name,
@@ -132,7 +133,7 @@ module.exports = {
 
   deleteByAdmin: (req, res)=>{
     var token = jwt.verify(req.header.token, 'krisis_only_need_to_handle_with_calm');
-    if(token.role === 'admin'){
+    if(token.role.toLowerCase() === 'admin'){
       models.User.destroy({
         where: {
           id: req.params.id
@@ -146,7 +147,7 @@ module.exports = {
 
   updateAuthor: (req, res)=>{
     var token = jwt.verify(req.header.token, 'krisis_only_need_to_handle_with_calm');
-    if(token.role === 'admin' || token.role === 'user'){
+    if(token.role.toLowerCase() === 'admin' || token.role.toLowerCase() === 'user'){
       let user = {
         name: req.body.name,
         username: req.body.username,
@@ -158,7 +159,7 @@ module.exports = {
         }
       })
       .then(()=>{
-        res.send(`Data id ${req.params.id} Has been changed` )
+        res.send(`Data id ${req.params.id} Has been changed`)
       })
     }else {
       res.send(`You'r not authorize to access the data`)
--- routes/api.js
+++ routes/api.js
@@ -10,16 +10,16 @@ router.get('/', (req, res, next)=>{
 })
 
 
-router.get('/users', userController.getAllUser)
-router.get('/users/:id', userController.findUser)
-router.post('/users', userController.insertUser)
-router.delete('/users/:id', userController.deleteUser)
-router.put('/users/:id', userController.updateUser)
+// router.get('/users', userController.getAllUser)
+// router.get('/users/:id', userController.findUser)
+// router.post('/users', userController.insertUser)
+// router.delete('/users/:id', userController.deleteUser)
+// router.put('/users/:id', userController.updateUser)
 router.post('/signup', userController.signUp)
 router.post('/signin', userController.signIn)
 router.get('/users', userController.getAllUserInfo)
 router.get('/users/:id', userController.getAuthor)
-router.post('/users', userController.insertUserByAdmin)
+router.post('/users/:token', userController.insertUserByAdmin)
 router.delete('/users/:id', userController.deleteByAdmin)
 router.put('/users/:id', userController.updateAuthor)
 
